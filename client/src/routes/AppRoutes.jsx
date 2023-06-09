f686d29cb61699314d2916064086dfff7fdb1965
moshe <moshe6073163@gmail.com>
2023-06-09 10:19:21 +0300

    Moshe

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import App from "../App";
 import NotFound from "../NotFound";
-import MainSite from "../comp/MainSite";
+import AddMissions from "../comp/sideNav/addMissions/AddMissions";
+import Dashboard from "../comp/sideNav/dashboard/Dashboard";
+import MissionExeption from "../comp/sideNav/missionExeption/MissionExeption";
+import PendingMissions from "../comp/sideNav/pendingMissions/PendingMissions";
+import TaskList from "../comp/sideNav/taskList/TaskList";
+import Filter from "../comp/topNav/filter/Filter";
+import Message from "../comp/topNav/message/Message";
+import PrintFile from "../comp/topNav/printFile/PrintFile";
+import Settings from "../comp/topNav/settings/Settings";
 
 export default function AppRoutes() {
   return (
@@ -10,6 +18,17 @@ export default function AppRoutes() {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/addMissions" element={<AddMissions />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/missionExeption" element={<MissionExeption />} />
+        <Route path="/PendingMissions" element={<PendingMissions />} />
+        <Route path="/taskList" element={<TaskList />} />
+        <Route path="/filter" element={<Filter />} />
+        <Route path="/message" element={<Message />} />
+        <Route path="/printFile" element={<PrintFile />} />
+        <Route path="/settings" element={<Settings />} />
+
+
       </Routes>
     </>
   );
