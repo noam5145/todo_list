b5bd3e9785d1a64c5e26dd9e55d054c825631575
noam5145 <109206563+noam5145@users.noreply.github.com>
2023-06-12 09:36:12 +0300

    base side nav & top nav

--- client/src/comp/sideNav/SideNav.jsx
+++ client/src/comp/sideNav/SideNav.jsx
@@ -1,34 +1,122 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FaBars, FaHourglassHalf } from 'react-icons/fa';
-import { AiOutlineDashboard } from 'react-icons/ai';
-import { MdOutlineCreateNewFolder } from 'react-icons/md';
-import { RiInsertRowTop } from 'react-icons/ri';
-import { BsListTask } from 'react-icons/bs';
-import "./sideNav.css"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars, FaHourglassHalf } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineClose, AiOutlineDashboard } from "react-icons/ai";
+import { MdOutlineCreateNewFolder } from "react-icons/md";
+import { RiInsertRowTop } from "react-icons/ri";
+import { BsListTask } from "react-icons/bs";
+import "./sideNav.css";
 
 export default function SideNav() {
   const [showSideNav, setShowSideNav] = useState(true);
   return (
     <>
-      <div className={showSideNav ? "col-3 side_Nav":"col-1 side_Nav"}>
-        <div className={showSideNav ? "d-flex justify-content-end mt-2":"d-flex justify-content-center mt-2"}>
-          <FaBars onClick={() => setShowSideNav(!showSideNav)} color='red' size={40} />
+      <div className={showSideNav ? "col-2 side_Nav" : "col-1 side_Nav"}>
+        <div
+          className={
+            showSideNav
+              ? "d-flex justify-content-end mx-2 mt-2"
+              : "d-flex justify-content-center mt-2"
+          }
+        >
+          <div className="mt-3" onClick={() => setShowSideNav(!showSideNav)}>
+            {!showSideNav ? (
+              <RxHamburgerMenu color="black" size={40} />
+            ) : (
+              <AiOutlineClose color="black" size={30} />
+            )}
+          </div>
         </div>
-        <div className='d-flex justify-content-center mt-4'>
-         {showSideNav ? <img height={200} width={200} src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/IDF_GOC_Army_Headquarters_From_2020_%28Alternative%29.svg/1200px-IDF_GOC_Army_Headquarters_From_2020_%28Alternative%29.svg.png' />
-          :<img height={70} width={70} src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/IDF_GOC_Army_Headquarters_From_2020_%28Alternative%29.svg/1200px-IDF_GOC_Army_Headquarters_From_2020_%28Alternative%29.svg.png' />}
+        <div className="d-flex justify-content-center mt-5">
+          <img
+            height={showSideNav ? "140" : "70"}
+            width={showSideNav ? "140" : "70"}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/IDF_GOC_Army_Headquarters_From_2020_%28Alternative%29.svg/1200px-IDF_GOC_Army_Headquarters_From_2020_%28Alternative%29.svg.png"
+          />
         </div>
-        <div className="mt-5">
-        <Link to={'/dashboard'} className='d-flex justify-content-center pl-0 pr-0 col-12 mb-4 nav-link'>{showSideNav &&<h4 className="d-flex align-items-center mb-0 mr-2">Dashboard</h4>}<div className="d-flex align-items-center"><AiOutlineDashboard size={35} /></div></Link>
-        <Link to={'/addMissions'} className='d-flex justify-content-center pl-0 pr-0 col-12 mb-4 nav-link'>{showSideNav &&<h4 className="d-flex align-items-center mb-0 mr-2">Add Missions</h4>}<div className="d-flex align-items-center"><MdOutlineCreateNewFolder size={35} /></div></Link>
-        <Link to={'/missionExeption'} className='d-flex justify-content-center pl-0 pr-0 col-12 mb-4 nav-link'>{showSideNav &&<h4 className="d-flex align-items-center mb-0 mr-2">Mission Exeption</h4>}<div className="d-flex align-items-center"><FaHourglassHalf size={35} /></div></Link>
-        <Link to={'/pendingMissions'} className='d-flex justify-content-center pl-0 pr-0 col-12 mb-4 nav-link'>{showSideNav &&<h4 className="d-flex align-items-center mb-0 mr-2">Pending Missions</h4>}<div className="d-flex align-items-center"><RiInsertRowTop size={35} /></div></Link>
-        <Link to={'/taskList'} className='d-flex justify-content-center pl-0 pr-0 col-12 mb-4 nav-link'>{showSideNav &&<h4 className="d-flex align-items-center mb-0 mr-2">ListTask</h4>}<div className="d-flex align-items-center"><BsListTask size={35} /></div></Link>
+        <div className="iconsNavSide">
+          <Link
+            to={"/dashboard"}
+            className={
+              showSideNav
+                ? "d-flex justify-content-start s_n_margin pl-0 pr-0 col-12 mb-4 nav-link s_n_hover "
+                : "d-flex justify-content-center pl-0 pr-0 col-12 mb-4 nav-link s_n_hover "
+            }
+          >
+            <AiOutlineDashboard className="iconNavSaid" size={showSideNav ? "25" : "40"} />
+
+            {showSideNav && (
+              <div className="d-flex align-items-center mb-0 s_n_margin">
+                {" "}
+                דשבורד משימות
+              </div>
+            )}
+          </Link>
+          <Link
+            to={"/addMissions"}
+            className={
+              showSideNav
+                ? "d-flex justify-content-start s_n_margin pl-0 pr-0 col-12 mb-4 nav-link s_n_hover "
+                : "d-flex justify-content-center pl-0 pr-0 col-12 mb-4 nav-link s_n_hover "
+            }
+          >
+            <MdOutlineCreateNewFolder className="iconNavSaid" size={showSideNav ? "25" : "40"} />
+            {showSideNav && (
+              <div className="d-flex align-items-center mb-0 s_n_margin">
+                מאגר משימות
+              </div>
+            )}
+          </Link>
+          <Link
+            to={"/missionExeption"}
+            className={
+              showSideNav
+                ? "d-flex justify-content-start s_n_margin pl-0 pr-0 col-12 mb-4 nav-link s_n_hover "
+                : "d-flex justify-content-center pl-0 pr-0 col-12 mb-4 nav-link s_n_hover "
+            }
+          >
+            <FaHourglassHalf className="iconNavSaid" size={showSideNav ? "25" : "40"} />
+            {showSideNav && (
+              <div className="d-flex align-items-center mb-0 s_n_margin">
+                דוח משימות בחריגה{" "}
+              </div>
+            )}
+          </Link>
+          <Link
+            to={"/pendingMissions"}
+            className={
+              showSideNav
+                ? "d-flex justify-content-start s_n_margin pl-0 pr-0 col-12 mb-4 nav-link s_n_hover "
+                : "d-flex justify-content-center pl-0 pr-0 col-12 mb-4 nav-link s_n_hover "
+            }
+          >
+            <RiInsertRowTop className="iconNavSaid" size={showSideNav ? "25" : "40"} />
+
+            {showSideNav && (
+              <div className="d-flex align-items-center mb-0 s_n_margin">
+                דוח ממתינות לאישור
+              </div>
+            )}
+          </Link>
+          <Link
+            to={"/taskList"}
+            className={
+              showSideNav
+                ? "d-flex justify-content-start s_n_margin pl-0 pr-0 col-12 mb-4 nav-link s_n_hover "
+                : "d-flex justify-content-center pl-0 pr-0 col-12 mb-4 nav-link s_n_hover "
+            }
+          >
+            <BsListTask className="iconNavSaid" size={showSideNav ? "25" : "40"} />
+
+            {showSideNav && (
+              <div className="d-flex align-items-center mb-0 s_n_margin">
+                הוספת משימות
+              </div>
+            )}
+          </Link>
         </div>
       </div>
     </>
-
-
-  )
+  );
 }
