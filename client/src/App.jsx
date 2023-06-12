9c5d26887cf70e43bd35a34fdc4c98a352c0f2f1
eliezerkolin <109139354+eliezerkolin@users.noreply.github.com>
2023-06-12 13:44:35 +0300

    base comp of the add mission

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,30 @@
 import React, { createContext, useState } from "react";
 import MainSite from "./comp/MainSite";
+import AppRoutes from "./routes/AppRoutes";
+import axios from 'axios';
 
 
+const base_url_mission = 'http://localhost:5174/mission';
 
 export const MyContext = createContext();
 
 
 export default function App() {
-  let val = {
+  const [currentUser, setCurrentUser]= useState({namg:''});
 
+
+  const newMission = async (mission)=>{
+    let res = await axios.post(base_url_mission, mission);
+    console.log(res.data);
+  }
+  const val = {
+    currentUser,
+    newMission,
   }
   return (
     <div>
       <MyContext.Provider value={val} >
-        <MainSite />
+      <AppRoutes/>
       </MyContext.Provider>
     </div>
   );
