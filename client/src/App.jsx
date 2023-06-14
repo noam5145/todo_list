7122f0287ec4e60abb661017cfa855ab2139c98b
moshe <moshe6073163@gmail.com>
2023-06-14 09:38:19 +0300

    Merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React, { createContext, useEffect, useState } from "react";
 import MainSite from "./comp/MainSite";
 import axios from 'axios';
 import AppRoutes from "./routes/AppRoutes";
+import Login from "./comp/Login";
 const base_url = 'https://server-todolist-xr2q.onrender.com/';
 
 export const MyContext = createContext();
@@ -9,26 +10,43 @@ export const MyContext = createContext();
 
 export default function App() {
   const [currentUser, setCurrentUser] = useState();
-  let [missions, setMissions]=useState();
+  const [missions, setMissions] = useState([]);
   const newMission = async(mission)=>{
-      let res = await axios.get(base_url + 'mission', {params: mission});
-      setMissions(res.data)
-      console.log(missions)
-    
-  
+    let res = await axios.get(base_url + 'mission', {params: mission});
   }
 
-  const setUser = async (username)=>{
-    let user = await axios.get(base_url + 'user/getUser', {params: {username : username}});
+  const setNewUser = async (user)=>{
+    let user1 = await axios.post(base_url + 'user/setUser', {...user,
+      adminToken: currentUser.token,
+    });
+    if(user.data.err){
+      return user.data.err;
+    }
     console.log(user.data);
   }
+
+  const getUser = async(user)=>{
+    let res = await axios.get(base_url + 'user/getUser', {params: user});
+    if(res.data.err){
+      return res.data.err;
+    }
+    setCurrentUser(res.data);
+  }
+
+  let flag = true;
+  useEffect(()=>{
+    if(flag){
+      // getUser()
+      console.log("M")
+      flag=false;
+    }
+  },[])
 
 
   let val = {
     currentUser,
     newMission,
-    missions,
-
+    getUser,
   }
 
 
@@ -36,6 +54,7 @@ export default function App() {
   return (
     <div>
       <MyContext.Provider value={val} >
+        {!currentUser ? <Login/> : ''}
         {currentUser?.username}
         <AppRoutes />
       </MyContext.Provider>
