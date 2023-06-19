81005fb548111054ea36f57a08fac5e0af7d6d4e
Nehoray <nehorayatia1@gmail.com>
2023-06-19 10:46:58 +0300

    exeption

--- src/comp/sideNav/missionExeption/LayoutMissionExeption/LayoutMissionExeption.jsx
+++ src/comp/sideNav/missionExeption/LayoutMissionExeption/LayoutMissionExeption.jsx
@@ -1,93 +1,128 @@
 
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./layoutMissionExeption.css";
-import UnfoldMoreIcon from "@mui/icons-material/UnfoldMore";
 
-import ReactToPrint, { ComponentToPrint,useReactToPrint } from "react-to-print";
+import { MyContext } from "../../../../App";
 
 
 const LayoutMissionExeption = React.forwardRef((props, ref) => {
     const [opemId, setOpemId] = useState(false);
+    const {missions}=useContext(MyContext);
+    const[exceptionMission,setExceptionMission]=useState(missions);
 
 
+console.log(missions);
 
     useEffect(() => {
       window.addEventListener("click", () => {
         setOpemId(false)
       })
+      let temp=exceptionMission.filter((item)=>{daysOff(item.endedAt)>0});
+      setExceptionMission(temp);
+      
+   
     }, [])
+    const date = new Date();
+
+ 
   
+    function daysOff(endTime) {
+      const date1 = new Date();
+    const date2 = new Date(endTime);
+    const diffTime =(date2-date1 );
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    
+  
+       return diffDays  ;
+    
+
+   
+    }
   
   
     return (
       <> 
-   
-   
       
         <div  ref={ref} className="container-fluid mt-2 mb-2">
           <div className="d-flex justify-content-between mx-5">
-            <div className="exp-title-div" >       
-                <h2 className="exp-title">דו"ח חריגה</h2>
+            <div className="exp-title-ex-div" >       
+                <h2 className="exp-title-ex">דו"ח חריגה</h2>
             </div>
           
-            <span className="">
+            <span>
             
             </span>
           </div>
-          <div className="container table-container-Ex all_table-Ex mt-3 ml-3">
-            <span className="sticky-top">
-            <div className=" d-flex justify-content-center">
+          <div className="container  table-container-Ex all_table-Ex mt-3 ml-3">
+            <span >
+            <div className=" d-flex justify-content-center sticky-top">
               <div className="col-1 top_table-Ex text-center">
-                מזהה <span title="מיין לפי גדול/קטן"><UnfoldMoreIcon className="cursor" /></span>
+                מזהה <span title="מיין לפי גדול/קטן"></span>
               </div>
-              <div className="col-2 top_table-Ex text-center" >
-              אחריות<span title="מיין לפי גדול/קטן"><UnfoldMoreIcon className="cursor" /></span>
+              <div className="col-1 top_table-Ex text-center" >
+              אחריות<span title="מיין לפי גדול/קטן"></span>
               </div>
-              <div className="col-2 top_table-Ex text-center">
-                כותרת הפגישה <span title="מיין לפי גדול/קטן"><UnfoldMoreIcon className="cursor" /></span>
+              <div className="col-1 top_table-Ex text-center">
+                כותרת הפגישה <span title="מיין לפי גדול/קטן"></span>
               </div>
               <div className="col-3 top_table-Ex text-center">
-                פירוט הפגישה <span title="מיין לפי גדול/קטן"><UnfoldMoreIcon className="cursor" /></span>
+                פירוט הפגישה <span title="מיין לפי גדול/קטן"></span>
               </div>
               <div className="col-1 top_table-Ex text-center">
-                תג"ב<span title="מיין לפי גדול/קטן"><UnfoldMoreIcon className="cursor" /></span>
+                תג"ב<span title="מיין לפי גדול/קטן"></span>
               </div>
               <div className="col-1 top_table-Ex text-center">
-                ימי חריגה<span title="מיין לפי גדול/קטן"><UnfoldMoreIcon className="cursor" /></span>
+                ימי חריגה<span title="מיין לפי גדול/קטן"></span>
               </div>
-              <div className="col-1 top_table-Ex text-center">
-                הערות אחראי<span title="מיין לפי גדול/קטן"><UnfoldMoreIcon className="cursor" /></span>
+              <div className="col-2 top_table-Ex text-center">
+                הערות אחראי<span title="מיין לפי גדול/קטן"></span>
               </div>
-              <div className="col-1 top_table-Ex text-center">
-                הערות מפקד<span title="מיין לפי גדול/קטן"><UnfoldMoreIcon className="cursor" /></span>
+              <div className="col-2 top_table-Ex text-center">
+                הערות מפקד<span title="מיין לפי גדול/קטן"></span>
               </div>
              
             </div>
   
          
             </span> 
-            {Array(10)
-              .fill(null)
-              .map((i, item) => (
-                <div key={i} className="container d-flex justify-content-center p-0">
-                  <div className="col-1 the_table-Ex text-center">135</div>
-                  <div className="col-2 the_table-Ex text-center">02/22/2023</div>
-                  <div className="col-2 the_table-Ex text-center">kjturyetr</div>
-                  <div className="col-3 the_table-Ex text-center align-items-center">
+            {missions.length>0?
+              missions
+              .map((mission, i) => (
+
+                
+
+                <div key={i} className="container-fluid d-flex justify-content-center p-0">
+                  <div className="col-1 the_table-Ex text-center">{mission.missionId}</div>
+                  <div className="col-1 the_table-Ex text-center">{mission.responsibility}</div>
+                  <div className="col-1 the_table-Ex text-center">{mission.title}</div>
+                  <div className="col-3 the_table-Ex text-center align-missions-center">
                     <p className="p_taskdetail-Ex p-2 ">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Provident odit quas rem. Accusantium, ducimus voluptatibus.
-                      Maiores eveniet at exercitationem ut iusto, dolorum
-                      voluptatibus aut eum rem labore sapiente facere consectetur!
+                     {mission.details}
                     </p>
                   </div>
-                  <div className="col-1 the_table-Ex  text-center">yiutyrt</div>
-                  <div className="col-1 the_table-Ex text-center">56</div>
-                  <div className="col-1 the_table-Ex  text-center">8</div>
-                  <div className="col-1 the_table-Ex  text-center"> בחריגה</div>
+                  <div className="col-1 the_table-Ex  text-center">{mission.endedAt}</div>
+                  <div className="col-1 the_table-Ex text-center">{daysOff(mission.endedAt)}</div>
+                  <div className="col-2 the_table-Ex  text-center">
+                  <p className="p_taskdetail-Ex p-2 ">
+                     {mission.noteResponsibility}
+                    </p>
+                    
+                   </div>
+                  <div className="col-2 the_table-Ex  text-center"> 
+                  <p className="p_taskdetail-Ex p-2 ">
+                     {mission.noteCommand}
+                    </p>
+                  
+                  </div>
                
                 </div>
-              ))}
+              ))
+            :<div className="col-12 d-flex justify-content-center mt-5 ">
+              <h2 style={{fontSize:"60px"}}> אין משימות בחריגה כעת</h2>
+
+            </div>
+              } 
+          
           </div>
         </div>
       </>
