9c123bfe55aef111b868e5de3304226aa54f8f58
noam5145 <109206563+noam5145@users.noreply.github.com>
2023-06-19 15:47:03 +0300

    ש

--- src/comp/topNav/userTasks/UserTasks.jsx
+++ src/comp/topNav/userTasks/UserTasks.jsx
@@ -14,9 +14,23 @@ export default function UserTasks() {
       );
     }
   }, [missions]);
+  console.log(missions);
 
   return (
-    <div>
+    <div className="container mt-5">
+      <h2>המשימות שלי </h2>
+
+      <div className=" row d-flex justify-content-center mt-5">
+        <div className="col-1 top_table text-center">מזהה</div>
+        <div className="col-1 top_table text-center">מועד משימה </div>
+        <div className="col-1 top_table text-center">כותרת משימה </div>
+        <div className="col-3 top_table text-center">פירוט משימה </div>
+        <div className="col-1 top_table text-center">מסמכים מצורפים</div>
+        <div className="col-1 top_table text-center">אחריות </div>
+        <div className="col-1 top_table text-center">תג"ב </div>
+        <div className="col-1 top_table text-center">ימים שנותרו </div>
+        <div className="col-1 top_table text-center">סטאטוס </div>
+      </div>
       {/* {
         listUserMissions.map((mission, i)=>(
           <div>
@@ -24,38 +38,25 @@ export default function UserTasks() {
           </div>
         ))
       } */}
-      <div className=" d-flex justify-content-center">
-        <div className="col-1 top_table text-center">מזהה</div>
-        <div className="col-1 top_table text-center">
-          מועד משימה{" "}
-        
-        </div>
-        <div className="col-1 top_table text-center">
-          כותרת משימה{" "}
-         
-        </div>
-        <div className="col-3 top_table text-center">
-          פירוט משימה{" "}
-          
-        </div>
-        <div className="col-1 top_table text-center">מסמכים מצורפים</div>
-        <div className="col-1 top_table text-center">
-          אחריות{" "}
-          
-        </div>
-        <div className="col-1 top_table text-center">
-          תג"ב{" "}
-          
-        </div>
-        <div className="col-1 top_table text-center">
-          ימים שנותרו{" "}
-         
-        </div>
-        <div className="col-1 top_table text-center">
-          סטאטוס{" "}
-       
-        </div>
-      </div>
+      {missions.map((mission, index)=>{
+        if(currentUser.token == mission.token){
+          return(
+            <div className="row d-flex justify-content-center">
+            <div className="col-1 border d-flex justify-content-center table_h">
+            {mission.missionId}
+            </div>
+            <div className="col-1 border d-flex justify-content-center text-center table_h">{mission.startedAt}</div>
+            <div className="col-1 border d-flex justify-content-center text-center table_h">{mission.title}</div>
+            <div className="col-3 col-1 border d-flex justify-content-center text-center table_h">{mission.details}</div>
+            <div className="col-1 border d-flex justify-content-center text-center table_h">---</div>
+            <div className="col-1 border d-flex justify-content-center text-center table_h">{mission.responsibility} </div>
+            <div className="col-1 border d-flex justify-content-center text-center table_h">{mission.endedAt}</div>
+            <div className="col-1 border d-flex justify-content-center text-center table_h">---</div>
+            <div className="col-1 border d-flex justify-content-center text-center table_h">{mission.status} </div>
+          </div>
+          )
+        }
+      })}
     </div>
   );
 }
