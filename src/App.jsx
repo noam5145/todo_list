473f5067b2b1f4b4d4adee9b1c9d818dd2fc73d6
moshe <moshe6073163@gmail.com>
2023-06-25 10:21:02 +0300

    m

--- src/App.jsx
+++ src/App.jsx
@@ -120,10 +120,7 @@ export default function App() {
 
   useEffect(()=>{
     if(flag){
-      let t;
-      if(typeof(document.cookie) === 'string') {
-        t = document.cookie.split('T_L_T=')[1]?.split(';')[0]
-      } 
+      let t = document.cookie.split('T_L_T=')[1]?.split(';')[0]
       if(t){
         getUser({token: t});
       }
