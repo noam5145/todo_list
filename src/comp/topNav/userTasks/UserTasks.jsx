177e1742a461df9831f5a6b693dd6a73e04213a6
moshe <moshe6073163@gmail.com>
2023-06-25 12:53:47 +0300

    m

--- src/comp/topNav/userTasks/UserTasks.jsx
+++ src/comp/topNav/userTasks/UserTasks.jsx
@@ -24,50 +24,52 @@ export default function UserTasks() {
         <h4 className="text-secondary">
       אין משימות להצגה 
       </h4 >
-      </div>):(<div className=" row d-flex justify-content-center mt-5">
-        <div className="col-1 top_table text-center">מזהה</div>
-        <div className="col-1 top_table text-center">מועד משימה </div>
-        <div className="col-1 top_table text-center">כותרת משימה </div>
-        <div className="col-3 top_table text-center">פירוט משימה </div>
-        <div className="col-1 top_table text-center">מסמכים מצורפים</div>
-        <div className="col-1 top_table text-center">אחריות </div>
-        <div className="col-1 top_table text-center">תג"ב </div>
-        <div className="col-1 top_table text-center">ימים שנותרו </div>
-        <div className="col-1 top_table text-center">סטאטוס </div>
-      </div>
-      {listUserMissions.map((mission, index) => {
-        return (
-          <div className="row d-flex justify-content-center">
-            <div className="col-1 border d-flex justify-content-center table_h">
-              {mission.missionId}
+      </div>):(
+      <>
+        <div className=" row d-flex justify-content-center mt-5">
+          <div className="col-1 top_table text-center">מזהה</div>
+          <div className="col-1 top_table text-center">מועד משימה </div>
+          <div className="col-1 top_table text-center">כותרת משימה </div>
+          <div className="col-3 top_table text-center">פירוט משימה </div>
+          <div className="col-1 top_table text-center">מסמכים מצורפים</div>
+          <div className="col-1 top_table text-center">אחריות </div>
+          <div className="col-1 top_table text-center">תג"ב </div>
+          <div className="col-1 top_table text-center">ימים שנותרו </div>
+          <div className="col-1 top_table text-center">סטאטוס </div>
+        </div>
+        {listUserMissions.map((mission, index) => (
+            <div className="row d-flex justify-content-center">
+              <div className="col-1 border d-flex justify-content-center table_h">
+                {mission.missionId}
+              </div>
+              <div className="col-1 border d-flex justify-content-center text-center table_h">
+                {mission.startedAt}
+              </div>
+              <div className="col-1 border d-flex justify-content-center text-center table_h">
+                {mission.title}
+              </div>
+              <div className="col-3 col-1 border d-flex justify-content-center text-center table_h">
+                {mission.details}
+              </div>
+              <div className="col-1 border d-flex justify-content-center text-center table_h">
+                ---
+              </div>
+              <div className="col-1 border d-flex justify-content-center text-center table_h">
+                {mission.responsibility}{" "}
+              </div>
+              <div className="col-1 border d-flex justify-content-center text-center table_h">
+                {mission.endedAt}
+              </div>
+              <div className="col-1 border d-flex justify-content-center text-center table_h">
+                ---
+              </div>
+              <div className="col-1 border d-flex justify-content-center text-center table_h">
+                {mission.status}{" "}
+              </div>
             </div>
-            <div className="col-1 border d-flex justify-content-center text-center table_h">
-              {mission.startedAt}
-            </div>
-            <div className="col-1 border d-flex justify-content-center text-center table_h">
-              {mission.title}
-            </div>
-            <div className="col-3 col-1 border d-flex justify-content-center text-center table_h">
-              {mission.details}
-            </div>
-            <div className="col-1 border d-flex justify-content-center text-center table_h">
-              ---
-            </div>
-            <div className="col-1 border d-flex justify-content-center text-center table_h">
-              {mission.responsibility}{" "}
-            </div>
-            <div className="col-1 border d-flex justify-content-center text-center table_h">
-              {mission.endedAt}
-            </div>
-            <div className="col-1 border d-flex justify-content-center text-center table_h">
-              ---
-            </div>
-            <div className="col-1 border d-flex justify-content-center text-center table_h">
-              {mission.status}{" "}
-            </div>
-          </div>
-        );
-      })}
+        ))}
+      </>
+      )}
     </div>
   );
 }
